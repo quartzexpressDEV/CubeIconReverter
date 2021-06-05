cfb34211982c18f75ae0b058458658b189bd1e7e
Marco <52253797+quartzexpressDEV@users.noreply.github.com>
2021-06-05 02:16:28 +0200

    Projektdateien hinzufügen.

---
+++ Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿
+namespace CubeIconReverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.revert = new System.Windows.Forms.Button();
+            this.reset = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(414, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This Program will revert the changes made to lobby items in the latest update";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(411, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Clicking REVERT will revert the icons, clicking RESET will reset them to default";
+            // 
+            // revert
+            // 
+            this.revert.Location = new System.Drawing.Point(13, 43);
+            this.revert.Name = "revert";
+            this.revert.Size = new System.Drawing.Size(98, 40);
+            this.revert.TabIndex = 2;
+            this.revert.Text = "REVERT";
+            this.revert.UseVisualStyleBackColor = true;
+            this.revert.Click += new System.EventHandler(this.revert_Click);
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(329, 43);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(98, 40);
+            this.reset.TabIndex = 3;
+            this.reset.Text = "RESET";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(118, 43);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 15);
+            this.label3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(439, 95);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.revert);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CubeIconReverter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button revert;
+        private System.Windows.Forms.Button reset;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CubeIconReverter
+{
+    public partial class Form1 : Form
+    {
+        readonly string cache = Environment.GetEnvironmentVariable("localappdata") + "\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\localcache\\minecraftpe\\packcache\\";
+        readonly string json = "";
+        readonly string downloadurl = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void revert_Click(object sender, EventArgs e)
+        {
+            label1.Text = packdir();
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+
+        }
+        private string packdir()
+        {
+            string[] folders = Directory.GetFiles(cache);
+            foreach(string fold in folders)
+            {
+                Console.WriteLine(fold);
+                if (File.Exists($"{fold}\\manifest.json") && File.ReadAllText($"{fold}\\manifest.json") == json)
+                {
+                    return fold;
+                }
+            }
+            return null;
+        }
+    }
+}
