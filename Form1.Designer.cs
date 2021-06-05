4596307adfc3cf512b2f1e9fff7dbfcaed1cba54
Marco <52253797+quartzexpressDEV@users.noreply.github.com>
2021-06-05 14:43:31 +0200

    hello thjis works now pls dad love me

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,68 +29,88 @@ namespace CubeIconReverter
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.revert = new System.Windows.Forms.Button();
-            this.reset = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.customrp_checkbox = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 9);
+            this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(414, 15);
+            this.label1.Size = new System.Drawing.Size(413, 60);
             this.label1.TabIndex = 0;
-            this.label1.Text = "This Program will revert the changes made to lobby items in the latest update";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 24);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(411, 15);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Clicking REVERT will revert the icons, clicking RESET will reset them to default";
-            // 
-            // revert
-            // 
-            this.revert.Location = new System.Drawing.Point(13, 43);
-            this.revert.Name = "revert";
-            this.revert.Size = new System.Drawing.Size(98, 40);
-            this.revert.TabIndex = 2;
-            this.revert.Text = "REVERT";
-            this.revert.UseVisualStyleBackColor = true;
-            this.revert.Click += new System.EventHandler(this.revert_Click);
-            // 
-            // reset
-            // 
-            this.reset.Location = new System.Drawing.Point(329, 43);
-            this.reset.Name = "reset";
-            this.reset.Size = new System.Drawing.Size(98, 40);
-            this.reset.TabIndex = 3;
-            this.reset.Text = "RESET";
-            this.reset.UseVisualStyleBackColor = true;
-            this.reset.Click += new System.EventHandler(this.reset_Click);
+            this.label1.Text = resources.GetString("label1.Text");
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(118, 43);
+            this.label3.ForeColor = System.Drawing.Color.MediumOrchid;
+            this.label3.Location = new System.Drawing.Point(12, 243);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(0, 15);
-            this.label3.TabIndex = 4;
+            this.label3.Size = new System.Drawing.Size(253, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "just a status thing im too lazy to make invisible";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(12, 217);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(195, 23);
+            this.start.TabIndex = 3;
+            this.start.Text = "START";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(231, 217);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(195, 23);
+            this.clear.TabIndex = 4;
+            this.clear.Text = "CLEAR";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // customrp_checkbox
+            // 
+            this.customrp_checkbox.AutoSize = true;
+            this.customrp_checkbox.Checked = true;
+            this.customrp_checkbox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.customrp_checkbox.Enabled = false;
+            this.customrp_checkbox.Location = new System.Drawing.Point(13, 76);
+            this.customrp_checkbox.Name = "customrp_checkbox";
+            this.customrp_checkbox.Size = new System.Drawing.Size(125, 19);
+            this.customrp_checkbox.TabIndex = 5;
+            this.customrp_checkbox.Text = "Remove new icons";
+            this.customrp_checkbox.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(12, 102);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(282, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Healthbars instead of hearts in damage indicator";
+            this.checkBox1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(439, 95);
+            this.ClientSize = new System.Drawing.Size(438, 267);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.customrp_checkbox);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.start);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.reset);
-            this.Controls.Add(this.revert);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
@@ -107,10 +127,11 @@ namespace CubeIconReverter
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button revert;
-        private System.Windows.Forms.Button reset;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button start;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.CheckBox customrp_checkbox;
+        private System.Windows.Forms.CheckBox checkBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,44 +4,64 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CubeIconReverter.Properties;
 
 namespace CubeIconReverter
 {
     public partial class Form1 : Form
     {
-        readonly string cache = Environment.GetEnvironmentVariable("localappdata") + "\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\localcache\\minecraftpe\\packcache\\";
-        readonly string json = "";
-        readonly string downloadurl = "";
+        private readonly string cachepath = $"{Environment.GetEnvironmentVariable("localappdata")}\\Packages\\Microsoft.MinecraftUWP_8wekyb3d8bbwe\\localcache\\minecraftpe\\packcache\\resource";
+        private readonly string url = "https://reinindieol.ga/cdn/cubeiconreverter/packs";
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void revert_Click(object sender, EventArgs e)
+        private void start_Click(object sender, EventArgs e)
         {
-            label1.Text = packdir();
+            clearcache();
+            dl_pack();
         }
 
-        private void reset_Click(object sender, EventArgs e)
+        private void clear_Click(object sender, EventArgs e)
         {
-
+            clearcache();
         }
-        private string packdir()
+        private void clearcache()
         {
-            string[] folders = Directory.GetFiles(cache);
-            foreach(string fold in folders)
+            try
             {
-                Console.WriteLine(fold);
-                if (File.Exists($"{fold}\\manifest.json") && File.ReadAllText($"{fold}\\manifest.json") == json)
-                {
-                    return fold;
-                }
+                Directory.Delete(cachepath, true);
+                Directory.CreateDirectory(cachepath);
+                label3.Text = "cleared cache";
             }
-            return null;
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                label3.Text = "error";
+            }
+            
+        }
+        private void dl_pack()
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                wc.DownloadFile($"{url}/default.zip", $"{cachepath}\\default.zip");
+                ZipFile.ExtractToDirectory($"{cachepath}\\default.zip", $"{cachepath}\\anticcpack");
+                label3.Text = "downloaded pack";
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e);
+                label3.Text = "error";
+            }
         }
     }
 }
