d4d99c895559e4fa6c29e180782a6eae8a93b521
Marco <52253797+quartzexpressDEV@users.noreply.github.com>
2021-06-11 20:26:42 +0200

    commented updater

--- Form1.cs
+++ Form1.cs
@@ -86,6 +86,8 @@ namespace CubeIconReverter
         }
         private void Form1_Load(object sender, EventArgs e)
         {
+            //this will be uncommented in the release but commented in nightly builds
+            /*
             Updater.DeleteOldVersion();
             Modules.Get().ForEach((name) => hbNames.Add(name));
             if (version != Updater.Get().tag_name) {
@@ -95,6 +97,7 @@ namespace CubeIconReverter
                     Updater.Update();
                 }
             };
+            */
             update.Text = Updater.version;
             hbSelectBtn.Hide();
         }
